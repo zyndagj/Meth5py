bab44e9f33df1999a812a7da14b0b986f5105fc1
gjzynda <gjzynda@indiana.edu>
2013-08-27 23:37:51 -0500

    Packaging with docs

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+
+"""
+Setup script for pymethyl
+"""
+
+from distutils.core import setup
+
+setup(name = "pymethyl",
+	version = "0.1",
+	author = "Greg Zynda",
+	author_email="gjzynda@indiana.edu",
+	license="GNU",
+	description = "Indexing of MethylCoder output",
+	packages = ["pymethyl"])

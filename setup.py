c6debafb27a7469f75a818c51453b0652bc38380
Greg Zynda <gzynda@login1.stampede.tacc.utexas.edu>
2014-11-07 11:29:56 -0600

    updated version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from distutils.core import setup, Extension
 module1 = Extension('cFetch', sources=["pymethyl/cFetch.c"])
 
 setup(name = "pymethyl",
-	version = "0.1",
+	version = "0.2",
 	author = "Greg Zynda",
 	author_email="gjzynda@indiana.edu",
 	license="GNU",
